3b1945930cd168ab5379f457f951a92d2d1b86c3
mattrandom <44298255+mattrandom@users.noreply.github.com>
2024-01-09 13:08:42 +0100

    [Order-Service] Add missing @Transactional annotation

--- order-service/src/main/java/com/github/mattrandom/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/github/mattrandom/orderservice/service/OrderService.java
@@ -8,11 +8,13 @@ import com.github.mattrandom.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 
 @Slf4j
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class OrderService {
 
